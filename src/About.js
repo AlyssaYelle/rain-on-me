22043bd6f37228d804b046328c9dfa6a94375550
Alyssa <alyssadanijones@gmail.com>
2019-11-16 11:20:36 -0500

    added info to about + some minor styling

--- src/About.js
+++ src/About.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function About() {
   return (
-    <div></div>
+    <div className='about'>
+      <h2>Hi there! Welcome to my weather app!</h2>
+      <p>
+        This app uses JavaScript's Geolocation API to determine a user's
+      current latitude/longitude coordinates, and then sends those coordinates
+      to the Dark Sky API, which then returns the local weather report.
+      </p>
+      <p>
+        Currently, Rain On Me only shows the user their current temperature, plus the high and low temperatures for the day.
+        My next will be to use D3.js to to visualize the weekly weather report!
+      </p>
+      <p>
+        You can find the code for Rain On Me on my github, <a href="https://github.com/AlyssaYelle/rain-on-me">here</a>.
+        If you want to skip straight to the juicy parts, the
+        bulk of the functionality of this app is in the <a href="https://github.com/AlyssaYelle/rain-on-me/blob/master/src/Weather.js">Weather.js file.</a>
+      </p>
+    </div>
   )
 }
 
